c09f9318a8e1077c85082dcc88402696edf56aa4
Hexiee <136283192+Hexiee@users.noreply.github.com>
2023-06-15 15:21:24 -0300

    Ultimo

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 
 export function TodoItem({ todo, cambiarEstado, eliminarTareas }) {
-  const { id, task, completed} = todo;
-  const fnCambiarEstado = () =>{
-    cambiarEstado(id)
-  };
-  const handleDeleteClick = () => {
-    eliminarTareas();
+  const { id, task, completed } = todo;
+
+  const handleOnChange = () => {
+    cambiarEstado(id);
   };
 
+  const handleEliminarTareas = () => {
+    eliminarTareas(id);
+  };
 
   return (
-    <li className="list-group-item">
-      <input type="checkbox" className="form-checked-input me-2" onChange={fnCambiarEstado} checked={completed} onClick={handleDeleteClick}/>
-      {task}
+    <li className={list-group-item ${completed ? 'bg-success text-white' : 'bg-danger text-white'}}>
+      <div className="form-check">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          checked={completed}
+          onChange={handleOnChange}
+        />
+        <label className="form-check-label">{task}</label>
+      </div>
+      <button
+        className="btn btn-danger btn-sm ms-2"
+        onClick={handleEliminarTareas}
+      >
+        <i className="bi bi-trash"></i>
+      </button>
     </li>
   );
 }
