6bd95b941dd0e6fffb7334317a456a1fba5a0024
Hexiee <136283192+Hexiee@users.noreply.github.com>
2023-06-15 15:22:41 -0300

    Ultimo2

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,33 +1,29 @@
 import React from "react";
 
-export function TodoItem({ todo, cambiarEstado, eliminarTareas }) {
+export function TodoItem({ todo, cambiarEstado }) {
   const { id, task, completed } = todo;
 
   const handleOnChange = () => {
     cambiarEstado(id);
   };
 
-  const handleEliminarTareas = () => {
-    eliminarTareas(id);
+  const listItemStyle = {
+    backgroundColor: completed ? "lightgreen" : "orange",
+    color: "#fff",
+    padding: "10px",
+    margin: "5px 0",
+    borderRadius: "5px",
   };
 
   return (
-    <li className={list-group-item ${completed ? 'bg-success text-white' : 'bg-danger text-white'}}>
-      <div className="form-check">
-        <input
-          className="form-check-input"
-          type="checkbox"
-          checked={completed}
-          onChange={handleOnChange}
-        />
-        <label className="form-check-label">{task}</label>
-      </div>
-      <button
-        className="btn btn-danger btn-sm ms-2"
-        onClick={handleEliminarTareas}
-      >
-        <i className="bi bi-trash"></i>
-      </button>
+    <li style={listItemStyle}>
+      <input
+        className="form-check-input me-2"
+        type="checkbox"
+        checked={completed}
+        onChange={handleOnChange}
+      />
+      {task}
     </li>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useRef, useState } from "react";
+import React, { Fragment, useRef, useState } from "react"
 
 /* Generar id unicas */
 import uuid4 from "uuid4";
